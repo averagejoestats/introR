57c0fabeb8f320e1982df4c9a432cba382ebb785
Joe Guinness <averagejoestats@gmail.com>
2023-08-02 11:16:25 -0400

    first session

---
+++ video_code/first_session_code.R
@@ -0,0 +1,87 @@
+
+# How to watch and learn
+# - get 2 screens if you can
+# - open the <filename>_comments.R version in RStudio
+# - type in the commands yourself as you go
+# - hit pause and experiment beyond what we do here
+# - Read the help files. Yes, really read them.
+# - It will feel slow and tedious at first  --->  faster path to mastery
+
+
+## Part 1:
+
+# Install R
+# Install RStudio
+# Organize your files!
+# Rstudio layout
+# RStudio options
+# - disable workspace saving and storing
+# - code on left, console on right
+
+
+
+
+
+
+
+
+
+
+## Part 2:
+
+# this is a comment
+
+
+# understand your working directory - global vs local paths
+
+
+# read in a dataset (titanic data)
+# source: https://hbiostat.org/data/
+# also available at: 
+# https://raw.githubusercontent.com/averagejoestats/introR/main/datasets/titanic5.csv
+
+
+# csv and data.frame store a table of data
+# each row is an "observation" (one person)
+# each column is a "variable" (name, sex, etc.)
+# columns may have different types (character string, number, date)
+
+# get information about the dataset
+
+# why did ticket get coded as character?
+# what about commas in Name?
+
+
+# access a column
+
+
+# basic statistics
+
+
+# basic math operations
+
+
+# math operations on vectors
+
+
+# getting help
+
+
+# what happened with 'x' and 'digits'?
+
+
+# you can also typing commands directly in the console
+# same as running them from the script
+
+
+# create your own vectors
+
+
+# improve our plot of Survived vs Age by adding uniform noise
+# to each variable
+
+
+# save your data frame as a csv
+
+
+# saving your data frame as an RData object
