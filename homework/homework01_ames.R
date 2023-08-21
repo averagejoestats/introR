12a29760612c0d87c878a6ca3ee7fb0a8e05e2bf
Joe Guinness <averagejoestats@gmail.com>
2023-08-21 19:23:43 -0400

    homework material

---
+++ homework/homework01_ames.R
@@ -0,0 +1,76 @@
+
+# Add your code to complete the tasks outlined in each set of comments.
+# You should use code and variables whenever possible, instead
+# of hard-coding numbers into your script.
+
+
+# Create a folder on your computer for storing files for this
+# course. You should have different subfolders (subdirectories)
+# for different types of files (e.g. datasets, homework)
+# (no code needed for this part)
+
+# Set your working directory to the folder that contains this script.
+# Then print out your working directory.
+
+# Read in the file ames_housing.csv using relative paths. Assign it
+# to a variable with an appropriate name.
+
+
+# How many rows and columns are in the dataset?
+
+
+# Print out the first six rows of the data.
+
+
+# make a histogram of the Gr_Liv_Area variable (total square feet)
+# The histogram should have 50 bars
+
+
+# What are the different Bldg_Type's, and how many of each type
+# are there? This should take 1 line of code.
+
+
+# In what year was the oldest house built?
+
+
+# How large is the largest building (by Gr_Liv_Area)
+
+
+# Look up the documentation for the which.max function, and use it
+# to print out all of the data for the largest building
+
+
+# Make a scatter plot of the Sale_Price against Gr_Liv_Area
+
+
+# Calculate the average sale price
+
+
+# Calculate the median sale price
+
+
+# Look at the documentation for the quantile function, and use it
+# to calculate the 0.25, 0.5, 0.75, and 0.95 quantiles of Sale_Price,
+# all in one line of code
+
+
+# Calculate the *proportion* of lots that are slightly irregular
+# without hard-coding the denominator of the proportion.
+
+
+# Use the summary function to figure out if Gr_Liv_Area is always
+# equal to First_Flr_SF + Second_Flr_SF
+
+
+# Make a table counting all combinations of House_Style and the total 
+# number of bathrooms (half baths count as 1/2 of a bathroom)
+# make the table look as nice as possible
+
+
+# Print out the first 10 rows of the ames data and only the following columns: 
+# Bldg_Type, Year_Built, Gr_Liv_Area, Bedroom_AbvGr, Year_Sold, Sale_Price
+
+
+# Save a csv file of the dataset but only for the columns listed above.
+# Do not include row names
+
