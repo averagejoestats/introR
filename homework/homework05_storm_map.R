8fc3188ae3e2980fb1c6c9a530cbb49354205c40
Joe Guinness <averagejoestats@gmail.com>
2023-09-17 18:12:00 -0400

    homework 05 material

---
+++ homework/homework05_storm_map.R
@@ -0,0 +1,9 @@
+
+# Reproduce the plot in ../plots/storm_map_2022.pdf
+# as closely as you can.
+
+# Don't worry about reproducing the exact colors of the tracks.
+# But do use different colors for different tracks.
+
+# You should use the maps package to draw the maps.
+# Spend some time reading the documentation for the "map" function.
