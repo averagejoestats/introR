b72a7bc07ddda6e4080a76509bf78130d169437b
Joe Guinness <averagejoestats@gmail.com>
2023-09-25 09:27:17 -0400

    homework 06

---
+++ homework/homework06_marathon.R
@@ -0,0 +1,50 @@
+
+# read in the marathon data
+
+
+# do a regression of time_minutes on vaporfly and sex
+
+
+# give your interpretation of the results
+
+
+# the previous analysis controlled for sex, in case the men
+# were more (or less) likely to wear vaporflys than women were
+# we should really control for runner, in case faster runners
+# tend to wear vaporflys.
+# Make a dataframe with 4 columns: 
+# - name, 
+# - sex, 
+# - vapor_avg: average time in vaporfly shoes,
+# - nonvapor_avg: average time in non-vaporfly shoes.
+# There are different ways of doing this.
+# You'll have to do some coding to create this dataset
+
+
+# print out the rows for the men with the 10 fastest vapor_avg
+# print out the rows for the women with the 10 fastest vapor_avg
+
+
+# create a new column for the difference between each runner's
+# vapor_avg and nonvapor_avg
+
+
+# calculate the mean difference for men and for women
+
+
+# print out the rows for the 10 runners with the largest difference.
+# largest means: biggest improvement when switching to vaporflys
+
+
+# how many men and how many women ran races in both types of shoes?
+
+
+# make a plot of the differences as a function of each runner's
+# nonvapor_avg. Differentiate the points for men and women.
+# make sure your plot looks nice (axis labels, etc.)
+
+
+
+
+
+
